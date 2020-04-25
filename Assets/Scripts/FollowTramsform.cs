8de99ed039b61aaae6fab01f62716a76860cd2db
Jin-Ch-Cal <63160115+Jin-Ch-Cal@users.noreply.github.com>
2020-04-25 20:48:39 +0800

    SyncBuildingwithModel

---
+++ Assets/Scripts/FollowTramsform.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTramsform : MonoBehaviour
+{
+    public GameObject model;
+
+    UnityEngine.Vector3 deltaPosition;
+    UnityEngine.Vector3 bInitialPostion;
+
+
+    void Start()
+    {
+        bInitialPostion = transform.position;
+        deltaPosition = transform.position - model.transform.position;
+
+    }
+
+    void Update()
+    {
+        transform.rotation = model.transform.rotation; //Follow rotation.
+        transform.position = model.transform.position * 250 + deltaPosition * 250 - bInitialPostion * 249; //Follow position.
+        transform.localScale = model.transform.localScale * 250; //Follow scale.
+
+    }
+}
