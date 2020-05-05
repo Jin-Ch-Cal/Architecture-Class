dbad0781be9f9dcffd71a6cf07b13852814f2657
Jin-Ch-Cal <63160115+Jin-Ch-Cal@users.noreply.github.com>
2020-05-06 06:38:45 +0800

    #Correction

--- Assets/Scripts/MoveVertice.cs
+++ Assets/Scripts/MoveVertice.cs
@@ -4,20 +4,27 @@ using UnityEngine;
 
 public class MoveVertice : MonoBehaviour
 {
-    public Vector3 vMove = new Vector3(0, 0, 0);
+    private SketchFace sketchFace;
 
+    public Vector3 vMove = new Vector3(0, 0, 0);
     public float moveSpeed = 10f;
     public float turnSpeed = 50f;
 
+    GameObject sphere4;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        sketchFace = GetComponent<SketchFace>();
+        sphere4 = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere4.transform.localScale = new Vector3(0.02f, 0.02f, 0.02f);
     }
 
     // Update is called once per frame
     void Update()
     {
+        sphere4.transform.position = sketchFace.cubeP[4] + vMove;
+
         if (Input.GetKey(KeyCode.I))
             vMove += Vector3.forward * moveSpeed * Time.deltaTime;
         if (Input.GetKey(KeyCode.K))
@@ -31,4 +38,6 @@ public class MoveVertice : MonoBehaviour
         if (Input.GetKey(KeyCode.M))
             vMove -= Vector3.up * moveSpeed * Time.deltaTime;
     }
+
+    
 }
