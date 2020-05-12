5e069df8b29333a298e550e9bffe4c69a1902dbf
Jin-Ch-Cal <63160115+Jin-Ch-Cal@users.noreply.github.com>
2020-05-13 03:37:33 +0800

    FixedPlayerRotationBug

---
+++ Assets/Scripts/CityModelMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CityModelMove : MonoBehaviour
+{
+    public GameObject player;
+    Vector3 cInitialPosition;
+    Vector3 pInitialPosition;
+
+    void Awake()
+    {
+        cInitialPosition = transform.position;
+        pInitialPosition = player.transform.position;
+    }
+
+    void Update()
+    {
+        transform.position = player.transform.position - pInitialPosition + cInitialPosition;
+    }
+}
